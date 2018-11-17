11b3c8b06567fe3ed1cf58abf4a8831dfcb0abf3
GauPanisset <gauthier.panisset@depinfonancy.net>
2018-11-17 14:17:32 +0100

    heroku

---
+++ Routes/contact2.js
@@ -0,0 +1,23 @@
+const sgMail = require('@sendgrid/mail');
+const Express = require('express');
+const router = Express.Router();
+
+
+
+
+router.post('/', function (req, res) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const msg = {
+        to: 'burdipolis@gmail.com',
+        from: req.body.user,
+        subject: "Burdipolis Website Message : " + req.body.subject,
+        text: req.body.text,
+        html: "From: " + req.body.name + " " + req.body.firstname + "<br>" +
+            "User's email: " + req.body.user + "<br>" +
+            "Message: " + req.body.text,
+    };
+    sgMail.send(msg);
+
+});
+
+module.exports.router = router;
